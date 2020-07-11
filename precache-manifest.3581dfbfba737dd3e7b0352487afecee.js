7b6770318e67cfcf41fbd0151439af13ef9eaf5b
vasiliym96 <ionart3737@gmail.com>
2020-07-11 10:56:33 +0300

    Updates

---
+++ precache-manifest.3581dfbfba737dd3e7b0352487afecee.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "18d515157798801d19642d8fdfe64151",
+    "url": "/easygame/index.html"
+  },
+  {
+    "revision": "d4486a3cd5378f0b7ca4",
+    "url": "/easygame/static/css/main.a0f262bd.chunk.css"
+  },
+  {
+    "revision": "0be32c1422c5da9968d3",
+    "url": "/easygame/static/js/2.413f15a7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/easygame/static/js/2.413f15a7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d4486a3cd5378f0b7ca4",
+    "url": "/easygame/static/js/main.11582e5c.chunk.js"
+  },
+  {
+    "revision": "52a1c73e57374c5cd12e",
+    "url": "/easygame/static/js/runtime-main.b68724fd.js"
+  }
+]);
